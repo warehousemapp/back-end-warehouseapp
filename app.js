df22aec007c3e58d5fe0d64cff52d3d096a7d57d
Daniel Soares de Albuquerque <77938356+warehousemapp@users.noreply.github.com>
2021-05-07 22:21:18 -0300

    prettier

--- app.js
+++ app.js
@@ -1,112 +1,124 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
-const { GoogleSpreadsheet } = require('google-spreadsheet');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
 const app = express();
 
 app.use(cors());
 
 //Usuarios
-const userRoute = require('./routes/user-route');
+const userRoute = require("./routes/user-route");
 
 //BODY PARSER - EXPRESS LIDAR LIDAR COM REQUISIÇÕES URLENCODED, FACILITAR O ENVIO DE ARQUIVOS
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000
+  })
+);
 //BODY PARSER - EXPRESS LIDAR COM REQUISIÇÕES FORMATO JSON
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: "50mb" }));
 
-const doc = new GoogleSpreadsheet('1jVDqLQw3-3mQ4BB59cE7_kW07qInQsjQVCekdeNPa8A');
+const doc = new GoogleSpreadsheet(
+  "1jVDqLQw3-3mQ4BB59cE7_kW07qInQsjQVCekdeNPa8A"
+);
 
-app.get('/', async (req, res) => {
-	try {
-		await doc.useServiceAccountAuth(require('./credentials/google-sheets-api.json'));
-		await doc.loadInfo(); // Carrega as infos da planilha
+app.get("/", async (req, res) => {
+  try {
+    await doc.useServiceAccountAuth(
+      require("./credentials/google-sheets-api.json")
+    );
+    await doc.loadInfo(); // Carrega as infos da planilha
 
-		const sheet = doc.sheetsByIndex[0];
+    const sheet = doc.sheetsByIndex[0];
 
-		
-  //Total de registros no banco
-  let total=sheet.rowCount;
+    //Total de registros no banco
+    let total = sheet.rowCount;
 
-		//let limit = { limit: 50 };
+    //let limit = { limit: 50 };
 
-		let page = Number(req.query.page)||1;
-		let limit = Number(req.query.per_page)||10;
-		//let search = req.query.search
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.per_page) || 10;
+    //let search = req.query.search
 
-		const rows = await sheet.getRows({offset:(page*limit)-limit,limit:limit});
+    const rows = await sheet.getRows({
+      offset: page * limit - limit,
+      limit: limit
+    });
 
-		let lastRow = rows.length-1;
+    let lastRow = rows.length - 1;
 
-		console.log({ totalPreenchidas: lastRow });
+    console.log({ totalPreenchidas: lastRow });
 
-		//console.log(rows)
+    //console.log(rows)
 
-		const dados = rows
-			.map(({ ID, imagem, nome, rowNumber }) => {
-				return {
-					ID,
-					imagem,
-					nome,
-					rowNumber
-				};
-			})//.filter((item) => item.nome.toLowerCase().includes(search.toLowerCase()));
+    const dados = rows.map(({ ID, imagem, nome, rowNumber }) => {
+      return {
+        ID,
+        imagem,
+        nome,
+        rowNumber
+      };
+    }); //.filter((item) => item.nome.toLowerCase().includes(search.toLowerCase()));
 
-
-		lRow = dados.length;
-		//console.log({ filtrados: lRow, dados });
-		console.log({ Total: total });
-		res.status(200).json(dados);
-	} catch (error) {
-		res.status(400).json({ success: false });
-	}
+    lRow = dados.length;
+    //console.log({ filtrados: lRow, dados });
+    console.log({ Total: total });
+    res.status(200).json(dados);
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 });
 
-app.get('/user/:id', async (req, res) => {
-	try {
-		await doc.useServiceAccountAuth(require('./credentials/google-sheets-api.json'));
-		await doc.loadInfo(); // Carrega as infos da planilha
+app.get("/user/:id", async (req, res) => {
+  try {
+    await doc.useServiceAccountAuth(
+      require("./credentials/google-sheets-api.json")
+    );
+    await doc.loadInfo(); // Carrega as infos da planilha
 
-		const sheet = doc.sheetsByIndex[0];
+    const sheet = doc.sheetsByIndex[0];
 
-		let limit = { limit: 50 };
+    let limit = { limit: 50 };
 
-		let ID = req.params.id;
+    let ID = req.params.id;
 
-		const rows = await sheet.getRows();
+    const rows = await sheet.getRows();
 
-		let lastRow = rows.length;
+    let lastRow = rows.length;
 
-		console.log({ totalPreenchidas: lastRow });
+    console.log({ totalPreenchidas: lastRow });
 
-		//console.log(rows)
+    //console.log(rows)
 
-		const dados = rows
-			.map(({ ID, imagem, nome }) => {
-				return {
-					ID,
-					imagem,
-					nome
-				};
-			})
-			.filter((item) => item.ID === ID);
+    const dados = rows
+      .map(({ ID, imagem, nome }) => {
+        return {
+          ID,
+          imagem,
+          nome
+        };
+      })
+      .filter((item) => item.ID === ID);
 
-		lRow = dados.length;
-		//console.log({ filtrados: lRow });
-		console.log({ Total: total });
-		res.status(200).json(dados);
-	} catch (error) {
-		res.status(400).json({ success: false });
-	}
+    lRow = dados.length;
+    //console.log({ filtrados: lRow });
+    //console.log({ Total: total });
+    res.status(200).json(dados);
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 });
 
 //Usuarios
-app.use('/users', userRoute);
+app.use("/users", userRoute);
 
 //Server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-	console.log('Servidor rodando!');
+  console.log("Servidor rodando!");
 });
